7155e5b832c80bf8d0bfe4451d2c75c2dbb48877
diegolikescode <diegoprestes.git@hotmail.com>
2023-08-23 17:23:38 -0300

    fix: deploy.yml dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://diegolikescode.me",
-  base: "/",
   integrations: [
     tailwind(),
     image({
