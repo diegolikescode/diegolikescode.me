10d4628bb8987e781dd3280ac9f6a4335e4497c4
diegolikescode <diegoprestes.git@hotmail.com>
2023-08-23 17:20:32 -0300

    temp remove work section and config deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://diegolikescode.me",
+  base: "/",
   integrations: [
     tailwind(),
     image({
