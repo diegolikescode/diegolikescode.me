9a0bc38fa4d0301788125405b63590a133597057
diegolikescode <diegoprestes.git@hotmail.com>
2025-02-25 09:12:11 -0300

    new configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
-      },
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                // sans: [
+                //     'Inter Variable',
+                //     'Inter',
+                //     ...defaultTheme.fontFamily.sans,
+                // ],
+                sans: ['IBM Plex Mono', 'monospace'],
+            },
+        },
     },
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
+    plugins: [require('@tailwindcss/typography')],
+}
